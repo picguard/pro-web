9bf6d57694dfb298952a689b1dd1a5004efa40cf
kjxbyz <kjxbyz@users.noreply.github.com>
2025-05-05 05:41:36 +0000

    deploy: PicGuard/PicGuard@c551188d7924c6ae1db3f038a33a9f4157f32eb3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3166099233"
+    serviceWorkerVersion: "2962821695"
   }
 });
